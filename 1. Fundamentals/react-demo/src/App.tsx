1752599138136feaed03ea7044c9bd626bde3a9c
Ayeshan-Oshitha <149539603+Ayeshan-Oshitha@users.noreply.github.com>
2025-07-05 13:20:19 +0530

    Section 02 ( Building Components ) - Excersise - Showing an Alert

--- 1. Fundamentals/react-demo/src/App.tsx
+++ 1. Fundamentals/react-demo/src/App.tsx
@@ -1,12 +1,21 @@
+import { useState } from "react";
+import Alert from "./components/Alert";
 import Button from "./components/Button";
 
-const handleClick = () => {
-  console.log("Clicked");
-};
-
 function App() {
+  const [alertVisible, setAlertVisibility] = useState(false);
+
+  const handleClick = () => {
+    setAlertVisibility(true);
+  };
+
+  const handleClose = () => {
+    setAlertVisibility(false);
+  };
+
   return (
     <div>
+      {alertVisible && <Alert onClose={handleClose}>My alert</Alert>}
       <Button onClick={handleClick} color="secondary">
         My Button
       </Button>

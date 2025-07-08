1adb7b6c946c356659b189876c5488ec86af5f3d
Ayeshan-Oshitha <149539603+Ayeshan-Oshitha@users.noreply.github.com>
2025-07-09 00:03:42 +0530

    Section 04 ( Managing Component State ) - Excersise - Building an ExpandableText Component

--- 1. Fundamentals/react-demo/src/App.tsx
+++ 1. Fundamentals/react-demo/src/App.tsx
@@ -1,44 +1,24 @@
-import { useState } from "react";
+import ExpandableText from "./components/ExpandableText";
 
 function App() {
-  const [game, setGame] = useState({
-    id: 1,
-    player: {
-      name: "John",
-    },
-  });
-
-  const handleClick = () => {
-    setGame({ ...game, player: { ...game.player, name: "Bob" } });
-  };
-
-  const [pizza, setPizza] = useState({
-    name: "Spicy Pepporoni Pizza",
-    toppings: ["Mushroom"],
-  });
-
-  const handleAdd = () => {
-    setPizza({ ...pizza, toppings: [...pizza.toppings, "Cheese"] });
-  };
-
-  const [cart, setCart] = useState({
-    discount: 0.1,
-    items: [
-      { id: 1, title: "Product 1", quantity: 1 },
-      { id: 2, title: "Product 2", quantity: 1 },
-    ],
-  });
-
-  const handleEdit = () => {
-    setCart({
-      ...cart,
-      items: cart.items.map((item) =>
-        item.id === 1 ? { ...item, quantity: item.quantity + 1 } : item
-      ),
-    });
-  };
-
-  return <div> </div>;
+  return (
+    <div>
+      <ExpandableText maxChars={120}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti nisi
+        nam distinctio laboriosam, animi soluta doloremque commodi dolor iusto
+        alias dolores, quisquam itaque assumenda eos qui doloribus facilis
+        obcaecati magni maxime rerum, numquam molestias voluptatibus facere
+        tenetur! Dignissimos quaerat tempore quam vitae et, culpa minima impedit
+        voluptatibus molestiae, soluta amet error perferendis ipsa? Reiciendis
+        dolore, autem unde perferendis sapiente minus doloremque dolorum velit
+        provident nisi, ut excepturi assumenda! Praesentium quos laboriosam
+        accusantium veniam quae qui, vel quis illo consequuntur iusto vitae.
+        Pariatur expedita quod dignissimos ipsam sint fuga magni quasi, harum
+        ratione quisquam temporibus tempora, mollitia voluptates. Est, quia
+        sint?
+      </ExpandableText>
+    </div>
+  );
 }
 
 export default App;

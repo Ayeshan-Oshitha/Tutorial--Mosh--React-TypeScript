30773ad2253faa29523e606f05e73aa95151d14a
Ayeshan-Oshitha <149539603+Ayeshan-Oshitha@users.noreply.github.com>
2025-07-09 13:15:29 +0530

    Section 05 ( Building Forms ) - Building a Form

--- 1. Fundamentals/react-demo/src/App.tsx
+++ 1. Fundamentals/react-demo/src/App.tsx
@@ -1,22 +1,9 @@
-import ExpandableText from "./components/ExpandableText";
+import Form from "./components/Form";
 
 function App() {
   return (
     <div>
-      <ExpandableText maxChars={120}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti nisi
-        nam distinctio laboriosam, animi soluta doloremque commodi dolor iusto
-        alias dolores, quisquam itaque assumenda eos qui doloribus facilis
-        obcaecati magni maxime rerum, numquam molestias voluptatibus facere
-        tenetur! Dignissimos quaerat tempore quam vitae et, culpa minima impedit
-        voluptatibus molestiae, soluta amet error perferendis ipsa? Reiciendis
-        dolore, autem unde perferendis sapiente minus doloremque dolorum velit
-        provident nisi, ut excepturi assumenda! Praesentium quos laboriosam
-        accusantium veniam quae qui, vel quis illo consequuntur iusto vitae.
-        Pariatur expedita quod dignissimos ipsam sint fuga magni quasi, harum
-        ratione quisquam temporibus tempora, mollitia voluptates. Est, quia
-        sint?
-      </ExpandableText>
+      <Form />
     </div>
   );
 }

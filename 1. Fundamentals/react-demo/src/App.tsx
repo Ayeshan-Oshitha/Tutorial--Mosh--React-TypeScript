41407932f2969758b45d7f040830958aa4b3c213
Ayeshan-Oshitha <149539603+Ayeshan-Oshitha@users.noreply.github.com>
2025-07-10 10:04:41 +0530

    Section 05 ( Building Forms ) - Excersise ( Expense Tracker ) - Building ExpenseFilter

--- 1. Fundamentals/react-demo/src/App.tsx
+++ 1. Fundamentals/react-demo/src/App.tsx
@@ -1,13 +1,20 @@
 import { useState } from "react";
 import ExpenseList from "./expense-tracker/components/ExpenseList";
+import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
 
 function App() {
+  const [selectedCategory, setSelectedCategory] = useState("");
+
   const [expenses, setExpenses] = useState([
     { id: 1, description: "aaa", amount: 10, category: "Utilities" },
-    { id: 2, description: "bbb", amount: 10, category: "Utilities" },
+    { id: 2, description: "bbb", amount: 10, category: "Groceries" },
     { id: 3, description: "ccc", amount: 20, category: "Utilities" },
-    { id: 4, description: "ddd", amount: 10, category: "Utilities" },
+    { id: 4, description: "ddd", amount: 10, category: "Entertainment" },
   ]);
+
+  const visibleExpenses = selectedCategory
+    ? expenses.filter((e) => e.category === selectedCategory)
+    : expenses;
 
   const deleteExpense = (id: number) => {
     setExpenses(expenses.filter((e) => e.id !== id));
@@ -15,7 +22,12 @@ function App() {
 
   return (
     <div>
-      <ExpenseList expenses={expenses} onDelete={deleteExpense} />
+      <div className="mb-3">
+        <ExpenseFilter
+          onSelectCategory={(category) => setSelectedCategory(category)}
+        />
+      </div>
+      <ExpenseList expenses={visibleExpenses} onDelete={deleteExpense} />
     </div>
   );
 }

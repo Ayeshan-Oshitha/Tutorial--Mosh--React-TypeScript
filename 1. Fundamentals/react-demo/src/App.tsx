8866e1b1141236c15a31de326a44e202a1a7bd8b
Ayeshan-Oshitha <149539603+Ayeshan-Oshitha@users.noreply.github.com>
2025-07-10 09:49:45 +0530

    Section 05 ( Building Forms ) - Excersise ( Expense Tracker ) -  Building ExpenseList

--- 1. Fundamentals/react-demo/src/App.tsx
+++ 1. Fundamentals/react-demo/src/App.tsx
@@ -1,9 +1,21 @@
-import Form from "./components/Form";
+import { useState } from "react";
+import ExpenseList from "./expense-tracker/components/ExpenseList";
 
 function App() {
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "aaa", amount: 10, category: "Utilities" },
+    { id: 2, description: "bbb", amount: 10, category: "Utilities" },
+    { id: 3, description: "ccc", amount: 20, category: "Utilities" },
+    { id: 4, description: "ddd", amount: 10, category: "Utilities" },
+  ]);
+
+  const deleteExpense = (id: number) => {
+    setExpenses(expenses.filter((e) => e.id !== id));
+  };
+
   return (
     <div>
-      <Form />
+      <ExpenseList expenses={expenses} onDelete={deleteExpense} />
     </div>
   );
 }

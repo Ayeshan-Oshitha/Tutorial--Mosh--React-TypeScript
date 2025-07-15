7aba1bddd8fac6560e3f275d51958d718534c87c
Ayeshan-Oshitha <149539603+Ayeshan-Oshitha@users.noreply.github.com>
2025-07-15 17:37:28 +0530

    Project (Fundamentals) - Displaying Platform Icons

--- 2. Project/rawg-game-hub/src/components/GameCard.tsx
+++ 2. Project/rawg-game-hub/src/components/GameCard.tsx
@@ -1,5 +1,6 @@
 import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
 import { Game } from "../hooks/useGames";
+import PlatformIconList from "./PlatformIconList";
 
 interface Props {
   game: Game;
@@ -8,10 +9,13 @@ interface Props {
 const GameCard = ({ game }: Props) => {
   return (
     <div>
-      <Card borderRadius={10} overflow="hidden" height="250px">
-        <Image src={game.background_image} height="175px" />
+      <Card borderRadius={10} overflow="hidden">
+        <Image src={game.background_image} />
         <CardBody>
           <Heading fontSize="2xl">{game.name}</Heading>
+          <PlatformIconList
+            platforms={game.parent_platforms.map((p) => p.platform)}
+          />
         </CardBody>
       </Card>
     </div>
--- 2. Project/rawg-game-hub/src/hooks/useGames.ts
+++ 2. Project/rawg-game-hub/src/hooks/useGames.ts
@@ -2,10 +2,16 @@ import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
 
+export interface Platform {
+  id: number;
+  name: string;
+  slug: string;
+}
 export interface Game {
   id: number;
   name: string;
   background_image: string;
+  parent_platforms: { platform: Platform }[]; // Smelly Code -> Should be  "Platform[]"
 }
 
 interface FetchGameResponse {

496a7b8e0866358afbad159ab1c8e54be6781684
Ayeshan-Oshitha <149539603+Ayeshan-Oshitha@users.noreply.github.com>
2025-07-15 17:47:52 +0530

    Project (Fundamentals) - Displaying Critic Score

--- 2. Project/rawg-game-hub/src/hooks/useGames.ts
+++ 2. Project/rawg-game-hub/src/hooks/useGames.ts
@@ -12,6 +12,7 @@ export interface Game {
   name: string;
   background_image: string;
   parent_platforms: { platform: Platform }[]; // Smelly Code -> Should be  "Platform[]"
+  metacritic: number;
 }
 
 interface FetchGameResponse {

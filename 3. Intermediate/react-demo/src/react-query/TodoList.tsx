f33442a4f680bdb295c9689504bf4af156e80c13
Ayeshan-Oshitha <149539603+Ayeshan-Oshitha@users.noreply.github.com>
2025-07-22 11:41:33 +0530

    Section 07 ( React Query - Fetching Data ) - Showing a Loading Indicator

--- 3. Intermediate/react-demo/src/react-query/TodoList.tsx
+++ 3. Intermediate/react-demo/src/react-query/TodoList.tsx
@@ -18,10 +18,16 @@ const TodoList = () => {
       });
   };
 
-  const { data: todos, error } = useQuery<Todo[], Error>({
+  const {
+    data: todos,
+    error,
+    isLoading,
+  } = useQuery<Todo[], Error>({
     queryKey: ["todos"],
     queryFn: fetchTodos,
   });
+
+  if (isLoading) return <p>Loading...</p>;
 
   if (error) return <p>{error.message}</p>;
 

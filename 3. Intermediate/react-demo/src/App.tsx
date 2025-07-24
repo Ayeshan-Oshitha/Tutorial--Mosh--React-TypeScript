fc0801afbceb35f0de46a064305b97de53cacc2a
Ayeshan-Oshitha <149539603+Ayeshan-Oshitha@users.noreply.github.com>
2025-07-24 10:57:10 +0530

    Section 08 ( Global State Management ) - Excersise - Working with Context

--- 3. Intermediate/react-demo/src/App.tsx
+++ 3. Intermediate/react-demo/src/App.tsx
@@ -3,19 +3,25 @@ import "./App.css";
 import LoginStatus from "./state-management/LoginStatus";
 import tasksReducer from "./state-management/reducers/tasksReducer";
 import NavBar from "./state-management/NavBar";
-
 import TasksContext from "./state-management/contexts/tasksContext";
 import HomePage from "./state-management/HomePage";
+import loginReducer from "./state-management/reducers/loginReducer";
+import AuthContext from "./state-management/contexts/authContext";
 
 function App() {
-  const [tasks, dispatch] = useReducer(tasksReducer, []);
+  const [tasks, tasksDispatch] = useReducer(tasksReducer, []);
+  const [user, authDispatch] = useReducer(loginReducer, "");
 
   return (
     <>
-      <TasksContext.Provider value={{ tasks: tasks, dispatch: dispatch }}>
-        <NavBar />
-        <HomePage />
-      </TasksContext.Provider>
+      <AuthContext.Provider value={{ user: user, dispatch: authDispatch }}>
+        <TasksContext.Provider
+          value={{ tasks: tasks, dispatch: tasksDispatch }}
+        >
+          <NavBar />
+          <HomePage />
+        </TasksContext.Provider>
+      </AuthContext.Provider>
     </>
   );
 }

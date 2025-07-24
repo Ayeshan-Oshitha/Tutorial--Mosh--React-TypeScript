4758ce21efe2b8522b3b9e711632c5a74b4bf41e
Ayeshan-Oshitha <149539603+Ayeshan-Oshitha@users.noreply.github.com>
2025-07-24 14:14:58 +0530

    Section 08 ( Global State Management ) - Excersise - Creating a Provider

--- 3. Intermediate/react-demo/src/App.tsx
+++ 3. Intermediate/react-demo/src/App.tsx
@@ -1,23 +1,17 @@
-import { useReducer } from "react";
 import "./App.css";
-import tasksReducer from "./state-management/reducers/tasksReducer";
 import NavBar from "./state-management/NavBar";
-import TasksContext from "./state-management/contexts/tasksContext";
 import HomePage from "./state-management/HomePage";
 import AuthProvider from "./state-management/AuthProvider";
+import TaskProvider from "./state-management/TaskProvider";
 
 function App() {
-  const [tasks, tasksDispatch] = useReducer(tasksReducer, []);
-
   return (
     <>
       <AuthProvider>
-        <TasksContext.Provider
-          value={{ tasks: tasks, dispatch: tasksDispatch }}
-        >
+        <TaskProvider>
           <NavBar />
           <HomePage />
-        </TasksContext.Provider>
+        </TaskProvider>
       </AuthProvider>
     </>
   );
---
+++ 3. Intermediate/react-demo/src/state-management/TaskProvider.tsx
@@ -0,0 +1,21 @@
+import React, { ReactNode, useReducer } from "react";
+import tasksReducer from "./reducers/tasksReducer";
+import TasksContext from "./contexts/tasksContext";
+
+interface Props {
+  children: ReactNode;
+}
+
+const TaskProvider = ({ children }: Props) => {
+  const [tasks, dispatch] = useReducer(tasksReducer, []);
+
+  return (
+    <>
+      <TasksContext.Provider value={{ tasks: tasks, dispatch: dispatch }}>
+        {children}
+      </TasksContext.Provider>
+    </>
+  );
+};
+
+export default TaskProvider;

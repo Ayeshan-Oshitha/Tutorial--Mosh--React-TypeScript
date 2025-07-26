27ab328987d0bdb36a6345eecdc59a4828c40299
Ayeshan-Oshitha <149539603+Ayeshan-Oshitha@users.noreply.github.com>
2025-07-26 15:30:40 +0530

    Project (Intermediate - React Query ) - Creating a reusable API Client

--- 4. Project/rawg-game-hub/src/services/api-client.ts
+++ 4. Project/rawg-game-hub/src/services/api-client.ts
@@ -1,13 +1,30 @@
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 
 export interface FetchResponse<T> {
   count: number;
   results: T[];
 }
 
-export default axios.create({
+const axiosInstance = axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
     key: "1939a1173ffe46f090aec6b31471cd3a",
   },
 });
+
+class APIClient<T> {
+  endpoint: string;
+
+  constructor(endpoint: string) {
+    this.endpoint = endpoint;
+  }
+
+  // Had to declare as arrow function. So doesn't have a problem with this keyword
+  getAll = (config: AxiosRequestConfig) => {
+    return axiosInstance
+      .get<FetchResponse<T>>(this.endpoint, config)
+      .then((res) => res.data.results);
+  };
+}
+
+export default APIClient;
